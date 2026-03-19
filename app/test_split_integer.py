c999566225dd6950d374d19e1fd84dcbe85ccfc0
Grimm <grimm60832@gmail.com>
2026-03-19 16:24:42 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -24,3 +24,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     result = split_integer(1, 3)
     assert result == [0, 0, 1]
+
+def test_max_difference_between_parts_should_be_at_most_1() -> None:
+    result = split_integer(17, 4)
+    assert max(result) - min(result) <= 1
